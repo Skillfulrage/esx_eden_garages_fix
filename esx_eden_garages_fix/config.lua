ac84c4dccc217c4bf8c4c4d29c044ff90c9652a1
Skillfulrage <51717174+Skillfulrage@users.noreply.github.com>
2019-10-25 22:37:10 -0500

    Add files via upload

--- esx_eden_garages_fix/config.lua
+++ esx_eden_garages_fix/config.lua
@@ -9,7 +9,7 @@ Config = {
 }
 
 Config.Garages = {
-	Garage_Centre = {	
+	Garage_0 = {	
 		Pos = {x=215.800, y=-810.057, z=29.727},
 		Size  = {x = 3.0, y = 3.0, z = 1.0},
 		Color = {r = 204, g = 204, b = 0},
@@ -17,14 +17,174 @@ Config.Garages = {
 		SpawnPoint = {
 			Pos = {x=229.700, y= -800.1149, z= 29.5722},
 			Color = {r=0,g=255,b=0},
-			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
 			Heading=157.84,
 			Marker = 1		
 		},
 		DeletePoint = {
 			Pos = {x=215.124, y=-791.377, z=29.646},
 			Color = {r=255,g=0,b=0},
-			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_1 = {	
+		Pos = {x=471.61, y=-1107.43, z=29.2},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=478.82, y=-1113.25, z=29.2},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=172.96,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x=465.18, y=-1113.54, z=29.2},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_2 = {	
+		Pos = {x=1041.52, y=-787.11, z=58},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=1032.28, y=-777.81, z=29.2},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=44.11,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x=1041.77, y=-786.72, z=58},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_3 = {	
+		Pos = {x=-53.35, y=-1834.45, z=26.58},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=-61.4, y=-1841.37, z=26.64},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=319.23,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x=-67.11, y=-1831.29, z=26.94},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_4 = {	
+		Pos = {x = 386.99,y = 285.49,z = 103.02},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 386.99,y = 285.49,z = 103.02},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=74.78,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x = 365.26,y = 286.96,z = 103.38},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_5 = {	
+		Pos = {x = -479.54,y = -614.38,z = 31.17},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -479.54,y = -614.38,z = 31.17},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=181.02,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x = -466.72,y = -618.87,z = 31.17},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_6 = {	
+		Pos = {x = 837.91,y = -1319.97,z = 26.18},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 837.91,y = -1319.97,z = 26.18},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=96.23,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x = 824.94,y = -1332.45,z = 26.11},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_6 = {	
+		Pos = {x = 837.91,y = -1319.97,z = 26.18},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 837.91,y = -1319.97,z = 26.18},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=96.23,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x = 824.94,y = -1332.45,z = 26.11},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Marker = 1,
+			
+		},
+	},
+	Garage_7 = {	
+		Pos = {x = -566.35,y = 317.83,z = 84.4},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -566.35,y = 317.83,z = 84.4},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
+			Heading=350.11,
+			Marker = 1		
+		},
+		DeletePoint = {
+			Pos = {x = -571.44,y = 329.65,z = 84.52},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 5.0, y = 5.0, z = 1.0},
 			Marker = 1,
 			
 		},
